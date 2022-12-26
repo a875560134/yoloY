511022e6a8d83f04688ac6ba1458b9f03d220672
rabbie <49755302+a875560134@users.noreply.github.com>
2022-12-26 18:24:02 +0800

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -344,7 +344,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
                 pass
 
         n = n_ = max(round(n * gd), 1) if n > 1 else n  # depth gain
-        if m in [Conv, CrossConv, DWConv, Conv6, DepthWiseConv, PointWiseConv, ConvSig, ConvSqu, RepConv, XBNConv,
+        if m in [Conv, CrossConv, DWConv, Conv6, DepthWiseConv, PointWiseConv, ConvSig, ConvSqu, RepConv, XBNConv, RobustConv, RobustConv2,
                  GhostConv, RepConv_OREPA, DepthSepConv, SimConv, CTR3,
                  SPP, SPPF, ASPP, CARAFE, SimSPPF, ECA, SE,ParNetAttention,
                  BottleneckCSP, BottleneckCSPA, BottleneckCSPB, BottleneckCSPC, BottleneckCSP2, BottleneckCSPF,
